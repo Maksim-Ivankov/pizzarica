aa69562d3a00baf90bf2fa79f4b95bafe02e8da7
MIN <maksimivankov26@yandex.ru>
2025-04-19 16:46:43 +0300

    111

--- src/database/database.py
+++ src/database/database.py
@@ -29,5 +29,47 @@ class Goods(db.Model):
        return {'id':self.id,'title':self.title,'description':self.description,'price':self.price,'img':self.img,'size':self.size,'category':self.category}
    
 
+class Orders(db.Model):
+   __tablename__ = 'orders'
+   id = db.Column(db.Integer, primary_key=True)
+   Name = db.Column(db.String(200)) # 
+   Phone = db.Column(db.String(200)) # 
+   Address = db.Column(db.Text) # 
+   goods = db.Column(db.Text) # 
 
+   @property
+   def serialize(self):
+      """Возвращайте данные объекта в легко сериализуемом формате"""
+      return {
+            'id':self.id,
+            'Name':self.Name,
+            'Phone':self.Phone,
+            'Address':self.Address,
+            'goods':self.goods,
+      }
+      
+   def __repr__(self):
+       return {'id':self.id,'Name':self.Name,'Phone':self.Phone,'Address':self.Address,'goods':self.goods,}
+   
+class Admin(db.Model):
+   __tablename__ = 'admin'
+   id = db.Column(db.Integer, primary_key=True)
+   login = db.Column(db.String(200)) # 
+   password = db.Column(db.String(200)) # 
+   role = db.Column(db.String(200)) # 
+
+
+   @property
+   def serialize(self):
+      """Возвращайте данные объекта в легко сериализуемом формате"""
+      return {
+            'id':self.id,
+            'login':self.login,
+            'password':self.password,
+            'role':self.role,
+      }
+      
+   def __repr__(self):
+       return {'id':self.id,'login':self.login,'password':self.password,'role':self.role,}
+   
 
